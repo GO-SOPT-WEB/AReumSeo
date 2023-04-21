197b5ec712a8883f1aba98b5222b6b8ac13dc2d2
Areum-lab <80264647+Areum-lab@users.noreply.github.com>
2023-04-21 15:08:52 +0900

    feat: 카테고리 선택 기능

--- Store/addNewItem.js
+++ Store/addNewItem.js
@@ -1,3 +1,11 @@
+const categoryData = [
+  { title: "===선택===", value: "none" },
+  { title: "전체🍳", value: "total" },
+  { title: "라면🍜", value: "ramen" },
+  { title: "간단안주🥠", value: "anju" },
+  { title: "술🍶", value: "soju" },
+];
+
 function handleFiles(input) {
   if (input.files && input.files[0]) {
     const reader = new FileReader();
@@ -9,3 +17,19 @@ function handleFiles(input) {
     reader.readAsDataURL(input.files[0]);
   }
 }
+
+function selectCategory() {
+  const createdSelect = document.querySelector(".selectCategory");
+  categoryData.forEach((data) => {
+    const createdOption = document.createElement("option");
+    createdOption.textContent = data.title;
+    createdOption.value = data.value;
+    createdSelect.appendChild(createdOption);
+  });
+}
+
+/* DOM */
+
+document.addEventListener("DOMContentLoaded", function () {
+  selectCategory();
+});
