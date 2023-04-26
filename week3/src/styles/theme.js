98e938110715a979b95fadbf51305aa8cac7ad6f
Areum-lab <80264647+Areum-lab@users.noreply.github.com>
2023-04-27 01:59:47 +0900

    style: 색상 및 폰트 수정

--- week3/src/styles/theme.js
+++ week3/src/styles/theme.js
@@ -8,6 +8,13 @@ font-weight: normal;
 font-style: normal;
 }
 
+@font-face {
+    font-family: 'LOTTERIACHAB';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2302@1.0/LOTTERIACHAB.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+}
+
 body {
     background-color: ${({ theme }) => theme.colors.yellow};
     font-family: 'UhBeeSe_hyun';
@@ -15,9 +22,7 @@ body {
 `;
 
 const colors = {
-  white: "#FFFFFF",
-  black: "#000000",
-  skyBlue: "#D4F0F0",
+  white: "#FFFFF0",
   lightPink: "#FEE1E8",
   lightPurple: "#ECD5E3",
   purple: "#CBAACB",
@@ -25,19 +30,15 @@ const colors = {
   yellow: "#FBDEA2",
   darkPink: "#FF968A",
   pink: "#E5B9B5",
-  lightSalmon: "#FED7C3",
   mint: "#C6DBDA",
   salmon: "#FCB9AA",
-  blue: "#8FCACA",
   lightGreen: "#CCE2CB",
   green: "#8EB695",
   darkGreen: "#2D4849",
-  aquaBlue: "#55CBCD",
-  lightAqua: "#A2E1DB",
 };
 
 const font = {
-  fontFamily: "UhBeeSe_hyun",
+  fontFamily: "LOTTERIACHAB",
 };
 
 const theme = {
