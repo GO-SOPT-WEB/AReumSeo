1affc685fbe1217bc4ead569d9863d555101c845
Areum-lab <80264647+Areum-lab@users.noreply.github.com>
2023-04-28 05:12:23 +0900

    chore: 카드 이미지 리스트 생성

---
+++ week3/src/constants/cardImgList.js
@@ -0,0 +1,48 @@
+import img_1 from "../assets/img_1.gif";
+import img_2 from "../assets/img_2.gif";
+import img_3 from "../assets/img_3.gif";
+import img_4 from "../assets/img_4.gif";
+import img_5 from "../assets/img_5.gif";
+import img_6 from "../assets/img_6.gif";
+import img_7 from "../assets/img_7.gif";
+import img_8 from "../assets/img_8.gif";
+import img_9 from "../assets/img_9.gif";
+
+export const cardImgList = [
+  {
+    name: img_1,
+    alt: "첫 번째 카드",
+  },
+  {
+    name: img_2,
+    alt: "두 번째 카드",
+  },
+  {
+    name: img_3,
+    alt: "세 번째 카드",
+  },
+  {
+    name: img_4,
+    alt: "네 번째 카드",
+  },
+  {
+    name: img_5,
+    alt: "다섯 번째 카드",
+  },
+  {
+    name: img_6,
+    alt: "여섯 번째 카드",
+  },
+  {
+    name: img_7,
+    alt: "일곱 번째 카드",
+  },
+  {
+    name: img_8,
+    alt: "여덟 번째 카드",
+  },
+  {
+    name: img_9,
+    alt: "아홉 번째 카드",
+  },
+];
