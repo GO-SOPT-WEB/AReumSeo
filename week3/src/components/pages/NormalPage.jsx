24770bbc28cb2c5677d459a83feda9fe45d4249a
Areum-lab <80264647+Areum-lab@users.noreply.github.com>
2023-04-28 05:13:00 +0900

    feat: 난이도 별 페이지 생성

---
+++ week3/src/components/pages/NormalPage.jsx
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+import { cardImgList } from "../../constants/cardImgList";
+
+const NormalPage = () => {
+  const easyCardList = cardImgList.slice(0, 7);
+  const copiedCardList = easyCardList.concat(easyCardList);
+  return (
+    <CardContainer>
+      {copiedCardList.map((data, idx) => {
+        return (
+          <CardBg key={idx}>
+            <img key={data.id * idx} src={data.name} />
+          </CardBg>
+        );
+      })}
+    </CardContainer>
+  );
+};
+
+export default NormalPage;
+
+const CardContainer = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+
+  margin-top: 20rem;
+`;
+
+const CardBg = styled.article`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 15rem;
+  height: 17rem;
+  margin: 1rem;
+  border-radius: 2rem;
+  background-color: ${({ theme }) => theme.colors.lightPink};
+  box-shadow: 1rem 1rem 1rem ${({ theme }) => theme.colors.pink};
+`;
