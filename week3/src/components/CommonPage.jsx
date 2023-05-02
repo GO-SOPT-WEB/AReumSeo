c95df59267b95d73817c8c4be4383c8536fa0fff
Areum-lab <80264647+Areum-lab@users.noreply.github.com>
2023-05-02 14:55:17 +0900

    feat: 카드 생성 부분 컴포넌트로 분리

--- week3/src/components/CommonPage.jsx
+++ week3/src/components/CommonPage.jsx
@@ -1,50 +1,43 @@
 import styled from "styled-components";
 import { useEffect, useState } from "react";
+import SingleCard from "./SingleCard";
 
-const EasyPage = (cardList) => {
+const CommonPage = (cardList) => {
   const copiedCardList = Object.keys(cardList)
     .map((item) => cardList[item])
     .flat();
   copiedCardList.sort(() => Math.random() - 0.5);
 
-  const [shuffledList, setShuffledList] = useState(copiedCardList);
+  const [shuffledCard, setShuffledCard] = useState(copiedCardList);
 
   useEffect(() => {
-    setShuffledList(shuffledList);
+    setShuffledCard(shuffledCard);
   }, []);
+
+  const handleChoice = (card) => {
+    console.log("clicked!");
+    console.log(card);
+  };
 
   return (
     <CardContainer>
       {copiedCardList.map((data, idx) => {
-        return (
-          <CardBg key={idx}>
-            <img key={data.id * idx} src={data.name} />
-          </CardBg>
-        );
+        {
+          console.log(data);
+        }
+        return <SingleCard key={idx} data={data} handleChoice={handleChoice} />;
       })}
     </CardContainer>
   );
 };
 
-export default EasyPage;
+export default CommonPage;
 
-const CardContainer = styled.section`
+const CardContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
   align-items: center;
 
   margin-top: 20rem;
-`;
-
-const CardBg = styled.article`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 15rem;
-  height: 17rem;
-  margin: 1rem;
-  border-radius: 2rem;
-  background-color: ${({ theme }) => theme.colors.lightPink};
-  box-shadow: 1rem 1rem 1rem ${({ theme }) => theme.colors.pink};
 `;
