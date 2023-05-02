0c4064d03d214d88c02befb693972a9ece0091a2
Areum-lab <80264647+Areum-lab@users.noreply.github.com>
2023-05-02 17:00:06 +0900

    feat: 선택된 카드의 동일함 여부에 따라 콘솔 출력

--- week3/src/components/CommonPage.jsx
+++ week3/src/components/CommonPage.jsx
@@ -8,23 +8,31 @@ const CommonPage = (cardList) => {
     .flat();
   copiedCardList.sort(() => Math.random() - 0.5);
 
-  const [shuffledCard, setShuffledCard] = useState(copiedCardList);
-
-  useEffect(() => {
-    setShuffledCard(shuffledCard);
-  }, []);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
 
   const handleChoice = (card) => {
     console.log("clicked!");
-    console.log(card);
+
+    // 값이 null이 아니면 이미 해당 값은 선택되어 있다는 것
+    // choiceOne에 값이 할당되어 있다면 choiceTwo의 값을 업데이트
+    // choiceOne에 값이 할당되어 있지 않다면 choiceOne의 값을 업데이트
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
   };
+
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      if (choiceOne.name === choiceTwo.name) {
+        console.log("those cards match");
+      } else {
+        console.log("fail");
+      }
+    }
+  }, [choiceOne, choiceTwo]);
 
   return (
     <CardContainer>
       {copiedCardList.map((data, idx) => {
-        {
-          console.log(data);
-        }
         return <SingleCard key={idx} data={data} handleChoice={handleChoice} />;
       })}
     </CardContainer>
