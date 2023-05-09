4512b23fa3861e5d0d4d5bb9072676e0f809c61d
Areum-lab <80264647+Areum-lab@users.noreply.github.com>
2023-05-10 04:04:27 +0900

    chore: 페이지 연결

--- week4/src/pages/DetailCardPage.jsx
+++ week4/src/pages/DetailCardPage.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import PageLayout from "../components/PageLayout";
+import SearchWeather from "../components/SearchWeather";
 
 const DetailCardPage = () => {
   return (
     <PageLayout>
+      <SearchWeather/>
       <Outlet />
     </PageLayout>
   );
