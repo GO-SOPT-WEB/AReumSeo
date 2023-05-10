788c4ff1e772e83f949d39f3bfb9233b216fd944
Areum-lab <80264647+Areum-lab@users.noreply.github.com>
2023-05-10 19:27:11 +0900

    feat: 지역 이름이 바뀌면 날씨 찾을 수 있도록 컴포넌트 연결

--- week4/src/components/ErrorPage.jsx
+++ week4/src/components/ErrorPage.jsx
@@ -1,6 +1,16 @@
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import DetailDayInfo from "./DetailDayInfo";
+import DetailWeekInfo from "./DetailWeekInfo";
 import styled from "styled-components";
 
-const ErrorPage = () => {
+const ErrorPage = ({ cityId }) => {
+  const location = useLocation();
+
+  useEffect(() => {
+    location.pathname.includes("day") ? <DetailDayInfo /> : <DetailWeekInfo />;
+  }, [cityId]);
+
   return <ErrorMessage>Not Found</ErrorMessage>;
 };
 
