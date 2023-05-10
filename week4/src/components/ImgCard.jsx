3f6a2ded247fc73be3cb817461210a38edccf596
Areum-lab <80264647+Areum-lab@users.noreply.github.com>
2023-05-10 16:45:10 +0900

    feat: 카드에 날씨 정보 출력

--- week4/src/components/ImgCard.jsx
+++ week4/src/components/ImgCard.jsx
@@ -4,14 +4,19 @@ import default_Img from "../assets/default_Img.gif";
 
 const ImgCard = (props) => {
   const { data } = props;
+
+  const temp = data.main && data.main.temp;
+  const feels_like = data.main && data.main.feels_like;
+  const maxTemp = data.main && data.main.temp_max;
+  const minTemp = data.main && data.main.temp_min;
+  const clouds = (data.clouds && data.clouds.all) * 100;
+
   const weatherToday = JSON.stringify(
     data.weather && data.weather[0].description
   );
-
   const filteredDay = WEATHER_TYPE.filter(
     (it) => JSON.stringify(it.description) === weatherToday
   );
-
   const imgSrc = filteredDay && filteredDay[0] ? filteredDay[0].imgURL : "";
 
   const onErrorImg = (e) => {
@@ -24,6 +29,26 @@ const ImgCard = (props) => {
         <strong>{data.dt_txt ? data.dt_txt.slice(5, 10) : data.name}</strong>
       </ImgWrapperHeader>
       <img src={imgSrc} onError={onErrorImg} />
+      <WeatherInfoContainer>
+        <WeatherInfo>
+          <p>온도</p>
+          <p>{temp}</p>
+        </WeatherInfo>
+        <WeatherInfo>
+          <p>체감 온도</p>
+          <p>{feels_like}</p>
+        </WeatherInfo>
+        <WeatherInfo>
+          <p>최저/최고</p>
+          <p>
+            {minTemp}/{maxTemp}
+          </p>
+        </WeatherInfo>
+        <WeatherInfo>
+          <p>구름</p>
+          <p>{clouds}%</p>
+        </WeatherInfo>
+      </WeatherInfoContainer>
     </ImgWrapper>
   );
 };
@@ -47,4 +72,16 @@ const ImgWrapperHeader = styled.header`
   margin: 1.35rem 0rem;
   font-size: 1.5rem;
   color: ${({ theme }) => theme.colors.darkGreen};
+`;
+
+const WeatherInfoContainer = styled.ul`
+  display: grid;
+  width: 11rem;
+  margin: 0;
+  padding: 0rem 0.5rem;
+`;
+
+const WeatherInfo = styled.li`
+  display: flex;
+  justify-content: space-between;
 `;
