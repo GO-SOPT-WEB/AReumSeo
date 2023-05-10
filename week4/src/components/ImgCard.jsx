1c49948181b00fa8a30e2613abaad5321b44adda
Areum-lab <80264647+Areum-lab@users.noreply.github.com>
2023-05-10 16:32:29 +0900

    feat: 이미지 카드/ 헤더 추가

--- week4/src/components/ImgCard.jsx
+++ week4/src/components/ImgCard.jsx
@@ -1,7 +1,34 @@
+import styled from "styled-components";
+
 const ImgCard = (props) => {
   const { data } = props;
 
-  return <div>{data}</div>;
+  return (
+    <ImgWrapper>
+      <ImgWrapperHeader>
+        <strong>{data.dt_txt ? data.dt_txt.slice(5, 10) : data.name}</strong>
+      </ImgWrapperHeader>
+    </ImgWrapper>
+  );
 };
 
 export default ImgCard;
+
+const ImgWrapper = styled.article`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+  padding: 1rem;
+
+  background-color: ${({ theme }) => theme.colors.lightGreen};
+  border-radius: 1rem;
+  box-shadow: 0.3rem 0.3rem 0.3rem ${({ theme }) => theme.colors.darkGreen};
+`;
+
+const ImgWrapperHeader = styled.header`
+  margin: 1.35rem 0rem;
+  font-size: 1.5rem;
+  color: ${({ theme }) => theme.colors.darkGreen};
+`;
