9a95ea0dbd887e0c6aadc0b7d85e0ca97451244d
Areum-lab <80264647+Areum-lab@users.noreply.github.com>
2023-05-11 01:06:28 +0900

    feat: 커스텀 훅 적용
    커스텀 훅 적용하여 기존 코드 대체

--- week4/src/components/DetailDayInfo.jsx
+++ week4/src/components/DetailDayInfo.jsx
@@ -1,38 +1,18 @@
-import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 import ImgCard from "./ImgCard";
 import ErrorPage from "./ErrorPage";
+import useDayFetch from "../hooks/useDayFetch";
 
 const DetailDayInfo = () => {
   const params = useParams();
   const { cityId } = params;
 
-  const [detailCardList, setdetailCardList] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  const getDetailCardInfo = () => {
-    setIsLoading(true);
-    fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${cityId}&appid=${
-        import.meta.env.VITE_APP_WEATHER
-      }&units=metric`
-    )
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        if (data.cod === 200) {
-          setdetailCardList(data);
-          setTimeout(() => setIsLoading(false), 1000);
-        }
-      })
-      .catch((err) => console.log(err));
-  };
-
-  useEffect(() => {
-    getDetailCardInfo();
-  }, [cityId]);
+  const [detailCardList, isLoading] = useDayFetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${cityId}&appid=${
+      import.meta.env.VITE_APP_WEATHER
+    }&units=metric`
+  );
 
   return (
     <CardListWrapper>
