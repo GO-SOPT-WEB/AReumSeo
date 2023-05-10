e732060399ba31a52d97c52b3803f24e52398c4a
Areum-lab <80264647+Areum-lab@users.noreply.github.com>
2023-05-10 19:22:14 +0900

    feat: 에러 페이지 초기 구현

---
+++ week4/src/components/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const ErrorPage = () => {
+  return <ErrorMessage>Not Found</ErrorMessage>;
+};
+
+export default ErrorPage;
+
+const ErrorMessage = styled.p`
+  font-size: 0.5rem;
+`;
