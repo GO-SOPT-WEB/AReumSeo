a526d3de1d670c714190097f2e736507135b0839
Areum-lab <80264647+Areum-lab@users.noreply.github.com>
2023-05-11 00:43:53 +0900

    feat: 주간 날씨 데이터 가져오는 커스텀 훅 생성

---
+++ week4/src/hooks/useWeekFetch.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+
+const useWeekFetch = (initialUrl) => {
+  const [detailCardList, setdetailCardList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getDetailCardInfo = () => {
+    setIsLoading(true);
+    fetch(initialUrl)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        if (data.cod === "200") {
+          setdetailCardList(
+            data.list.filter((it) => it.dt_txt.includes("00:00:00"))
+          );
+          console.log(detailCardList);
+          setTimeout(() => setIsLoading(false), 1000);
+        }
+      })
+      .catch((err) => console.log(err))
+      .finally(setIsLoading(false));
+  };
+
+  useEffect(() => {
+    getDetailCardInfo();
+  }, [initialUrl]);
+
+  return [detailCardList, isLoading];
+};
+
+export default useWeekFetch;
