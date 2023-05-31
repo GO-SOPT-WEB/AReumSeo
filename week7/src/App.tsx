44d60a43a55487c2872089fcba2a4a13548a28d8
Areum-lab <80264647+Areum-lab@users.noreply.github.com>
2023-06-01 01:28:39 +0900

    chore: 중복되는 부분 삭제

--- week7/src/App.tsx
+++ week7/src/App.tsx
@@ -1,10 +1,10 @@
-import { ThemeProvider, DefaultTheme } from "styled-components";
+import { ThemeProvider } from "styled-components";
 import theme from "./styles/theme";
 import { GlobalStyle } from "./styles/GlobalStyle";
 import Router from "./Router";
 function App() {
   return (
-    <ThemeProvider theme={theme as DefaultTheme}>
+    <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Router />
     </ThemeProvider>
