31a3a6ce98e39b2240f9e3aa1faa088a4c52ed47
Areum-lab <80264647+Areum-lab@users.noreply.github.com>
2023-06-06 02:17:06 +0900

    chore: 주석 삭제

--- week7/src/components/CommonPage.tsx
+++ week7/src/components/CommonPage.tsx
@@ -27,33 +27,27 @@ const CommonPage = (props: CommonPageProps) => {
 
   const flippedCard = document.getElementsByClassName("flipped");
 
-  // useMemo() 활용하여 cardList가 변경되지 않을 경우, 이전 값을 재사용하도록 구현
   const copiedCardList = useMemo(() => {
     setScore(0);
-    // JSON.parse(JSON.stringify(obj)): 깊은 복사
     const copied = JSON.parse(
       JSON.stringify(
-        // Object.keys(): 객체를 문자열 배열로 변환
         (Object.keys(cardList) as Array<keyof typeof cardList>)
           .map((item) => cardList[item])
-          // flat(): 하나의 배열로 만들고자 사용
           .flat()
       )
     );
-    // 카드 선택을 하다가 중간에 reset 버튼이나 레벨 선택을 다시 하면 이전에 저장되어 있던 카드 선택 정보 모두 삭제
     copied.map((it: cardImgListProps) => (it.matched = false));
 
     return copied.sort(() => Math.random() - 0.5);
   }, [cardList, shuffle]);
 
-  // 값이 null이 아니면 이미 해당 값은 선택되어 있다는 것
+
   const handleChoice = (card: cardImgListProps) => {
     choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
   };
 
   useEffect(() => {
     if (choiceOne && choiceTwo) {
-      // 두 개의 카드가 같은지 확인하는 동안 다른 카드 선택 불가
       setDisabled(true);
 
       if (choiceOne.name === choiceTwo.name) {
@@ -69,12 +63,10 @@ const CommonPage = (props: CommonPageProps) => {
     }
   }, [choiceOne, choiceTwo, flippedCard, copiedCardList]);
 
-  // 두 개의 카드가 선택된 후, 각 카드 정보 초기화
   const resetTurn = () => {
     setChoiceOne(null);
     setChoiceTwo(null);
 
-    // 검사가 끝나면 다른 카드 선택 가능
     setDisabled(false);
   };
 
