fe8093b771c0ca0c1f16c961e2b3b56a3882959b
Areum-lab <80264647+Areum-lab@users.noreply.github.com>
2023-06-06 02:13:27 +0900

    init: RecoilRoot 추가

--- week7/src/App.tsx
+++ week7/src/App.tsx
@@ -2,12 +2,16 @@ import { ThemeProvider } from "styled-components";
 import theme from "./styles/theme";
 import { GlobalStyle } from "./styles/GlobalStyle";
 import Router from "./Router";
+import { RecoilRoot } from "recoil";
+
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Router />
-    </ThemeProvider>
+    <RecoilRoot>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Router />
+      </ThemeProvider>
+    </RecoilRoot>
   );
 }
 
